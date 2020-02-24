c6169e1152e999873edb73da52e8b5656a7f4f27
leaf08 <leaf08@ya.ru>
2020-02-24 13:03:16 +0300

    feat: implement dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,25 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  teamNameArr = [];
+  teamNameStr = '';
+  count = 0;
+
+  if(!Array.isArray(members)) return false;
+
+  for(let str of members) {
+    if(typeof str == 'string') {
+      str = str.toUpperCase();
+      for(let char of str) {
+        if(char != ' ') {
+          teamNameArr[count] = char;
+          count++;
+          break;
+        }
+      }
+    }
+  }
+  teamNameArr.sort();
+  for(char of teamNameArr) {
+    teamNameStr += char;
+  }
+  return teamNameStr;
 };
