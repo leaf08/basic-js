ce71229c48198a1b226f0c123562b8e525ae50ac
leaf08 <leaf08@ya.ru>
2020-03-19 18:49:34 +0300

    feat: Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,73 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    direct = true;
+    constructor(direct) {
+        if(direct !== undefined) this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        if(str === undefined || key === undefined) throw 'Not implemented';
+        console.log(str + ' - ' + key);
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let iStr;
+        let iKey;
+        let result = '';
+        let resultReverse = '';
+        for(let i = 0, j = 0; i < str.length; i++) {
+            if('A' <= str[i] && str[i] <= 'Z') {
+                if(j < key.length) {
+                    iStr = this.a.indexOf(str[i]);
+                    iKey = this.a.indexOf(key[j]);
+                } else {
+                    iStr = this.a.indexOf(str[i]);
+                    iKey = this.a.indexOf(key[j % key.length]);
+                }
+                result += this.a[(iStr + iKey) % 26];
+                j++;
+            } else {
+                result += str[i];
+            }
+        }
+        if(this.direct === false) {
+            for(let i = result.length; i > -1; i--) {
+                resultReverse += result[i];
+            }
+            return resultReverse;
+        }
+        return result;
+    }
+
+    decrypt(str, key) {
+        if(str === undefined || key === undefined) throw 'Not implemented';
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let iStr;
+        let iKey;
+        let result = '';
+        let resultReverse = '';
+        for(let i = 0, j = 0; i < str.length; i++) {
+            if('A' <= str[i] && str[i] <= 'Z') {
+                if(j < key.length) {
+                    iStr = this.a.indexOf(str[i]);
+                    iKey = this.a.indexOf(key[j]);
+                } else {
+                    iStr = this.a.indexOf(str[i]);
+                    iKey = this.a.indexOf(key[j % key.length]);
+                }
+                result += this.a[(iStr + 26 - iKey) % 26];
+                j++;
+            } else {
+                result += str[i];
+            }
+        }
+        if(this.direct === false) {
+            for(let i = result.length; i > -1; i--) {
+                resultReverse += result[i];
+            }
+            return resultReverse;
+        }
+        return result;
     }
 }
 
