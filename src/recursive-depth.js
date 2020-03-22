5087cf230e912bfe794c7e71e4971b0b364f3caf
leaf08 <leaf08@ya.ru>
2020-03-22 08:59:09 +0300

    fix

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,18 +1,5 @@
 module.exports = class DepthCalculator {
-    depth = 0;
-    count = 0;
     calculateDepth(arr) {
-        for(let i = 0; i < arr.length; i++) {
-            if(Array.isArray(arr[i])) {
-                this.count++;
-                break;
-            }
-        }
-        if(this.count > 0) {
-            arr.flat();
-            this.depth++;
-            this.calculateDepth(arr);
-        }
-        return this.depth;
+        
     }
 };
